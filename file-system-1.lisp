0e3cd292c3dff2031363a9b29622c6b8052490ea
Mihir Mehta <mihirthegenius@gmail.com>
2017-02-09 12:24:44 -0600

    Certify file-system-1; move some lemmas outside

--- file-system-1.lisp
+++ file-system-1.lisp
@@ -1,4 +1,4 @@
-
+(in-package "ACL2")
 ;  file-system-1.lisp                                  Mihir Mehta
 
 ; Here we define the rudiments of a file system.  We first start with
@@ -6,49 +6,7 @@
 ; operations.
 
 (include-book "misc/assert" :dir :system)
-
-(defthm make-character-list-makes-character-list
-  (character-listp (make-character-list x)))
-
-(defthm make-character-list-makes-character-list
-  (character-listp (make-character-list x)))
-
-(defthm len-of-binary-append
-  (equal (len (binary-append x y)) (+ (len x) (len y))))
-
-(defthm len-of-make-character-list
-  (equal (len (make-character-list x)) (len x)))
-
-(defthm len-of-revappend
-  (equal (len (revappend x y)) (+ (len x) (len y))))
-
-(defthm len-of-first-n-ac
-  (implies (natp i) (equal (len (first-n-ac i l ac)) (+ i (len ac)))))
-
-(defthm nthcdr-of-binary-append-1
-  (implies (and (integerp n) (>= n (len x)))
-           (equal (nthcdr n (binary-append x y))
-                  (nthcdr (- n (len x)) y)))
-  :hints (("Goal" :induct (nthcdr n x)) ))
-
-(defthm first-n-ac-of-binary-append-1
-  (implies (and (natp i) (<= i (len x)))
-           (equal (first-n-ac i (binary-append x y) ac)
-                  (first-n-ac i x ac))))
-
-(defthm by-slice-you-mean-the-whole-cake
-  (implies (true-listp l)
-           (equal (first-n-ac (len l) l ac)
-                  (revappend ac l)))
-  :hints (("Goal" :induct (revappend l ac)) )
-  :rule-classes ((:rewrite :corollary
-                           (implies (and (true-listp l) (equal i (len l)))
-                                    (equal (first-n-ac i l ac) (revappend ac l)))) ))
-
-(defthm assoc-after-delete-assoc
-  (implies (not (equal name1 name2))
-           (equal (assoc-equal name1 (delete-assoc name2 alist))
-                  (assoc-equal name1 alist))))
+(include-book "file-system-lemmas")
 
 (defun fs-p (fs)
   (declare (xargs :guard t))
@@ -141,14 +99,6 @@
                        contents)
                 (cons (cons (car sd) (unlink (cdr hns) contents)) (delete-assoc (car hns) fs))))))))
     ))
-
-(defthm wrchs-guard-lemma-1
-  (implies (and (character-listp l) (character-listp acc) (<= n (len l)))
-           (character-listp (first-n-ac n l acc))))
-
-(defthm wrchs-guard-lemma-2
-  (implies (and (character-listp l))
-           (character-listp (nthcdr n l))))
 
 ; Add wrchs...
 ; The problem with this definition of wrchs is that it deletes a directory if
@@ -313,28 +263,28 @@ That takes care of that
 
 ; and so on...
 
-(assign fs '((a . "Mihir") (b . "Warren") (c (a . "Mehta") (b . "Hunt"))))
+;; (assign fs '((a . "Mihir") (b . "Warren") (c (a . "Mehta") (b . "Hunt"))))
 
-(assign h1 '(a))
-(assign h2 '(a b))
-(assign h3 '(c b))
-(assign h4 '(c))
+;; (assign h1 '(a))
+;; (assign h2 '(a b))
+;; (assign h3 '(c b))
+;; (assign h4 '(c))
 
-(stat (@ h1) (@ fs))
-(stat (@ h2) (@ fs))
-(stat (@ h3) (@ fs))
-(stat (@ h4) (@ fs))
+;; (stat (@ h1) (@ fs))
+;; (stat (@ h2) (@ fs))
+;; (stat (@ h3) (@ fs))
+;; (stat (@ h4) (@ fs))
 
-(wc-len (@ h1) (@ fs))
-(wc-len (@ h2) (@ fs))
-(wc-len (@ h3) (@ fs))
-(wc-len (@ h4) (@ fs))
+;; (wc-len (@ h1) (@ fs))
+;; (wc-len (@ h2) (@ fs))
+;; (wc-len (@ h3) (@ fs))
+;; (wc-len (@ h4) (@ fs))
 
-(wrchs (@ h1) (@ fs) 1 "athur")
-(wrchs (@ h3) (@ fs) 1 "inojosa")
-(wrchs (@ h3) (@ fs) 5 "Alvarez")
+;; (wrchs (@ h1) (@ fs) 1 "athur")
+;; (wrchs (@ h3) (@ fs) 1 "inojosa")
+;; (wrchs (@ h3) (@ fs) 5 "Alvarez")
 
-(unlink (@ h1) (@ fs))
-(unlink (@ h2) (@ fs))
-(unlink (@ h3) (@ fs))
-(unlink (@ h4) (@ fs))
+;; (unlink (@ h1) (@ fs))
+;; (unlink (@ h2) (@ fs))
+;; (unlink (@ h3) (@ fs))
+;; (unlink (@ h4) (@ fs))
---
+++ file-system-lemmas.lisp
@@ -0,0 +1,52 @@
+(in-package "ACL2")
+
+(defthm make-character-list-makes-character-list
+  (character-listp (make-character-list x)))
+
+(defthm make-character-list-makes-character-list
+  (character-listp (make-character-list x)))
+
+(defthm len-of-binary-append
+  (equal (len (binary-append x y)) (+ (len x) (len y))))
+
+(defthm len-of-make-character-list
+  (equal (len (make-character-list x)) (len x)))
+
+(defthm len-of-revappend
+  (equal (len (revappend x y)) (+ (len x) (len y))))
+
+(defthm len-of-first-n-ac
+  (implies (natp i) (equal (len (first-n-ac i l ac)) (+ i (len ac)))))
+
+(defthm nthcdr-of-binary-append-1
+  (implies (and (integerp n) (>= n (len x)))
+           (equal (nthcdr n (binary-append x y))
+                  (nthcdr (- n (len x)) y)))
+  :hints (("Goal" :induct (nthcdr n x)) ))
+
+(defthm first-n-ac-of-binary-append-1
+  (implies (and (natp i) (<= i (len x)))
+           (equal (first-n-ac i (binary-append x y) ac)
+                  (first-n-ac i x ac))))
+
+(defthm by-slice-you-mean-the-whole-cake
+  (implies (true-listp l)
+           (equal (first-n-ac (len l) l ac)
+                  (revappend ac l)))
+  :hints (("Goal" :induct (revappend l ac)) )
+  :rule-classes ((:rewrite :corollary
+                           (implies (and (true-listp l) (equal i (len l)))
+                                    (equal (first-n-ac i l ac) (revappend ac l)))) ))
+
+(defthm assoc-after-delete-assoc
+  (implies (not (equal name1 name2))
+           (equal (assoc-equal name1 (delete-assoc name2 alist))
+                  (assoc-equal name1 alist))))
+
+(defthm character-listp-of-first-n-ac
+  (implies (and (character-listp l) (character-listp acc) (<= n (len l)))
+           (character-listp (first-n-ac n l acc))))
+
+(defthm wrchs-guard-lemma-2
+  (implies (and (character-listp l))
+           (character-listp (nthcdr n l))))
