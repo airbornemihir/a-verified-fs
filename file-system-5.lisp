b53dc646b8dbdc70a12c3817d66fcf073a02dbc9
Mihir Mehta <mihir@cs.utexas.edu>
2017-12-01 11:44:59 -0600

    Prove read-after-write by any means necessary

--- file-system-5.lisp
+++ file-system-5.lisp
@@ -371,12 +371,33 @@
           (l5-regular-file-readable-p file user)
           (natp user))
      (equal
-      (l4-stat hns (l5-to-l4-fs fs) disk)
+      (l3-stat hns (l5-to-l4-fs fs) disk)
       (coerce
        (unmake-blocks
         (fetch-blocks-by-indices disk (l5-regular-file-contents file))
         (l5-regular-file-length file))
-       'string))))))
+       'string)))))
+  :rule-classes
+  (:rewrite
+   (:rewrite
+    :corollary
+    (implies
+     (and (symbol-listp hns)
+          (l5-fs-p fs)
+          (block-listp disk))
+     (let
+      ((file (l5-stat hns fs disk user)))
+      (implies
+       (and (l5-regular-file-entry-p file)
+            (l5-regular-file-readable-p file user)
+            (natp user))
+       (equal
+        (l4-stat hns (l5-to-l4-fs fs) disk)
+        (coerce
+         (unmake-blocks
+          (fetch-blocks-by-indices disk (l5-regular-file-contents file))
+          (l5-regular-file-length file))
+         'string))))))))
 
 (defthm
   l5-stat-correctness-2-lemma-1
@@ -389,15 +410,25 @@
 
 ;; This is the second of two theorems showing the equivalence of the l3 and l2
 ;; versions of stat.
-(defthm l5-stat-correctness-2
+(defthm
+  l5-stat-correctness-2
   (implies (and (symbol-listp hns)
                 (l5-fs-p fs)
                 (block-listp disk)
                 (natp user)
                 (l5-fs-p (l5-stat hns fs disk user)))
-           (equal (l4-stat hns (l5-to-l4-fs fs) disk)
-                  (l5-to-l4-fs (l5-stat hns fs disk user) )))
-  )
+           (equal (l3-stat hns (l5-to-l4-fs fs) disk)
+                  (l5-to-l4-fs (l5-stat hns fs disk user))))
+  :rule-classes
+  (:rewrite
+   (:rewrite
+    :corollary (implies (and (symbol-listp hns)
+                             (l5-fs-p fs)
+                             (block-listp disk)
+                             (natp user)
+                             (l5-fs-p (l5-stat hns fs disk user)))
+                        (equal (l4-stat hns (l5-to-l4-fs fs) disk)
+                               (l5-to-l4-fs (l5-stat hns fs disk user)))))))
 
 (defthm l5-wrchs-returns-fs-lemma-1
   (implies (l5-fs-p fs)
@@ -542,3 +573,212 @@
            
            (equal (l4-rdchs hns (l5-to-l4-fs fs) disk start n)
                   (l5-rdchs hns fs disk start n user))))
+
+(DEFTHM
+ L5-WRCHS-RETURNS-DISK-LEMMA-1
+ (IMPLIES
+  (L5-REGULAR-FILE-ENTRY-P ENTRY)
+      (equal
+           (BOUNDED-NAT-LISTP (BINARY-APPEND (L5-REGULAR-FILE-CONTENTS ENTRY)
+                                             Y)
+                              B)
+           (and (BOUNDED-NAT-LISTP (L5-REGULAR-FILE-CONTENTS ENTRY)
+                         B) (BOUNDED-NAT-LISTP y B)))
+      )
+ :hints (("Goal" :IN-THEORY (DISABLE BOUNDED-NAT-LISTP-CORRECTNESS-2) :USE (:INSTANCE BOUNDED-NAT-LISTP-CORRECTNESS-2
+                                 (X (L5-REGULAR-FILE-CONTENTS ENTRY))))))
+
+(defthm
+  l5-wrchs-returns-disk-lemma-2
+ (IMPLIES
+  (AND
+   (L5-FS-P FS)
+   (BOOLEAN-LISTP ALV)
+   (BOUNDED-NAT-LISTP (L4-LIST-ALL-INDICES (L5-TO-L4-FS FS))
+                      B))
+  (BOUNDED-NAT-LISTP
+   (L4-LIST-ALL-INDICES (L5-TO-L4-FS (CDR fs)))
+   B))
+ :hints (("Goal" :in-theory (enable L4-LIST-ALL-INDICES ))))
+
+(defthm
+  l5-wrchs-returns-disk-lemma-3
+ (IMPLIES
+  (AND
+   (CONSP (ASSOC-EQUAL NAME FS))
+   (L5-fs-p (CDR (ASSOC-EQUAL NAME FS)))
+   (L5-FS-P FS)
+   (BOOLEAN-LISTP ALV)
+   (BOUNDED-NAT-LISTP (L4-LIST-ALL-INDICES (L5-TO-L4-FS FS)) B))
+  (BOUNDED-NAT-LISTP
+   (L4-LIST-ALL-INDICES (L5-TO-L4-FS (CDR (ASSOC-EQUAL NAME FS)))) B))
+ :hints (("Goal" :in-theory (enable L4-LIST-ALL-INDICES )) ))
+
+(defthm
+  l5-wrchs-returns-disk-lemma-4
+  (IMPLIES
+   (AND
+    (CONSP (ASSOC-EQUAL NAME FS))
+    (L5-REGULAR-FILE-ENTRY-P (CDR (ASSOC-EQUAL NAME FS)))
+    (L5-FS-P FS)
+    (BOUNDED-NAT-LISTP (L4-LIST-ALL-INDICES (L5-TO-L4-FS FS)) B))
+   (BOUNDED-NAT-LISTP
+    (L5-REGULAR-FILE-CONTENTS (CDR (ASSOC-EQUAL NAME FS))) B))
+  :hints (("Goal" :in-theory (enable L4-LIST-ALL-INDICES L3-REGULAR-FILE-ENTRY-P)) ))
+
+(defthm
+  l5-wrchs-returns-disk
+  (implies
+   (and (l5-fs-p fs)
+        (stringp text)
+        (integerp start)
+        (<= 0 start)
+        (integerp user)
+        (<= 0 user)
+        (symbol-listp hns)
+        (block-listp disk)
+        (equal (len alv) (len disk))
+        (boolean-listp alv)
+        (bounded-nat-listp (l4-list-all-indices (l5-to-l4-fs fs))
+                           (len disk)))
+   (block-listp (mv-nth 1
+                        (l5-wrchs hns fs disk alv start text user))))
+  )
+
+(defthm
+  l5-read-after-write-1-lemma-1
+ (IMPLIES
+ (AND
+  (L5-FS-P FS)
+  (BOOLEAN-LISTP ALV)
+  (STRINGP TEXT)
+  (INTEGERP START)
+  (<= 0 START)
+  (SYMBOL-LISTP HNS)
+  (BLOCK-LISTP DISK)
+  (equal (len alv) (len disk))
+  (INTEGERP USER)
+  (<= 0 USER)
+  (L5-REGULAR-FILE-ENTRY-P (L5-STAT HNS FS DISK USER)))
+ (L5-REGULAR-FILE-ENTRY-P
+            (L5-STAT HNS
+                     (MV-NTH 0
+                             (L5-WRCHS HNS FS DISK ALV START TEXT USER))
+                     (MV-NTH 1
+                             (L5-WRCHS HNS FS DISK ALV START TEXT USER))
+                     USER)))
+ :hints (("Subgoal *1/5'''" :in-theory (disable l5-wrchs-returns-disk) :use
+          (:instance l5-wrchs-returns-disk (hns (cdr hns)) (fs
+                                        (CDR (ASSOC-EQUAL (CAR HNS) FS))))) ))
+
+(defthm
+  l5-read-after-write-1-lemma-2
+  (implies
+   (and (l5-fs-p fs)
+        (boolean-listp alv)
+        (stringp text)
+        (integerp start)
+        (<= 0 start)
+        (symbol-listp hns)
+        (block-listp disk)
+        (equal (len alv) (len disk))
+        (integerp user)
+        (<= 0 user))
+   (let
+    ((file (l5-stat hns fs disk user))
+     (new-file (l5-stat hns
+                        (mv-nth 0
+                                (l5-wrchs hns fs disk alv start text user))
+                        (mv-nth 1
+                                (l5-wrchs hns fs disk alv start text user))
+                        user)))
+    (implies (l5-regular-file-entry-p file)
+             (and (equal (l5-regular-file-user-read new-file)
+                         (l5-regular-file-user-read file))
+                  (equal (l5-regular-file-user new-file)
+                         (l5-regular-file-user file))
+                  (equal (l5-regular-file-other-read new-file)
+                         (l5-regular-file-other-read file)))))))
+
+(DEFTHM
+ L5-READ-AFTER-WRITE-1
+ (IMPLIES (AND (L5-FS-P FS)
+               (L4-STRICTER-FS-P (L5-TO-L4-FS FS) ALV)
+               (STRINGP TEXT)
+               (NATP START)
+               (SYMBOL-LISTP HNS)
+               (BOOLEAN-LISTP ALV)
+               (BLOCK-LISTP DISK)
+               (EQUAL (LEN ALV) (LEN DISK))
+               (NATP USER)
+               (<= (LEN (MAKE-BLOCKS (INSERT-TEXT NIL START TEXT)))
+                   (COUNT-FREE-BLOCKS ALV))
+               (EQUAL N (LENGTH TEXT))
+               (L5-REGULAR-FILE-ENTRY-P (L5-STAT HNS FS DISK USER))
+               (L5-REGULAR-FILE-READABLE-P (L5-STAT HNS FS DISK USER)
+                                           USER)
+               (L5-REGULAR-FILE-WRITABLE-P (L5-STAT HNS FS DISK USER)
+                                           USER))
+          (MV-LET (NEW-FS NEW-DISK NEW-ALV)
+                  (L5-WRCHS HNS FS DISK ALV START TEXT USER)
+                  (DECLARE (IGNORE NEW-ALV))
+                  (EQUAL (L5-RDCHS HNS NEW-FS NEW-DISK START N USER)
+                         TEXT)))
+ :INSTRUCTIONS
+ (:PROMOTE
+  :S-PROP
+  (:CLAIM
+   (EQUAL
+       (L5-RDCHS HNS
+                 (MV-NTH 0
+                         (L5-WRCHS HNS FS DISK ALV START TEXT USER))
+                 (MV-NTH 1
+                         (L5-WRCHS HNS FS DISK ALV START TEXT USER))
+                 START N USER)
+       (L4-RDCHS
+            HNS
+            (L5-TO-L4-FS (MV-NTH 0
+                                 (L5-WRCHS HNS FS DISK ALV START TEXT USER)))
+            (MV-NTH 1
+                    (L5-WRCHS HNS FS DISK ALV START TEXT USER))
+            START N))
+   :HINTS :NONE)
+  (:CHANGE-GOAL NIL T)
+  (:DIVE 2)
+  (:REWRITE L5-RDCHS-CORRECTNESS-1)
+  :TOP
+  :BASH :BASH :BASH :BASH :BASH (:DIVE 1)
+  := (:DROP 15)
+  (:DIVE 2)
+  :TOP
+  (:CLAIM
+   (AND
+     (EQUAL (L5-TO-L4-FS (MV-NTH 0
+                                 (L5-WRCHS HNS FS DISK ALV START TEXT USER)))
+            (MV-NTH 0
+                    (L4-WRCHS HNS (L5-TO-L4-FS FS)
+                              DISK ALV START TEXT)))
+     (EQUAL (MV-NTH 1
+                    (L5-WRCHS HNS FS DISK ALV START TEXT USER))
+            (MV-NTH 1
+                    (L4-WRCHS HNS (L5-TO-L4-FS FS)
+                              DISK ALV START TEXT))))
+   :HINTS :NONE)
+  (:DIVE 1 2)
+  := (:DROP 15)
+  :NX := (:DROP 15)
+  :TOP (:DIVE 1)
+  (:REWRITE L4-READ-AFTER-WRITE-1)
+  :TOP :BASH :BASH
+  (:IN-THEORY (DISABLE L5-WRCHS-CORRECTNESS-1))
+  (:USE L5-WRCHS-CORRECTNESS-1)
+  :BASH (:DIVE 2 2)
+  := (:DROP 1)
+  :TOP :BASH (:DIVE 2 2)
+  := (:DROP 1)
+  :TOP :BASH (:DIVE 2 2)
+  := (:DROP 1)
+  :TOP
+  :BASH (:DIVE 2 2)
+  := (:DROP 1)
+  :TOP :BASH))
