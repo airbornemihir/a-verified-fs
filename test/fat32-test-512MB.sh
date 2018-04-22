5a88a2223f2abf95d8c3556474a52361b9ae7d35
Mihir Mehta <mihir@cs.utexas.edu>
2018-04-22 08:06:30 -0500

    Rename

--- test/fat32-test-512MB.sh
+++ test/fat32-test-512MB.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# 512 MB was chosen because that's when mkfs.fat chooses to increase
+# the sectors-per-cluster value to 8, on its own accord, compared to
+# the value of 1 which is used for 256 MB.
 DISK=/tmp/disk1.raw
 MOUNTPOINT=/tmp/mount1
 SIZE=512M
