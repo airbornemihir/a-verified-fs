aaf42b90944f1d3ea5d5906867bfebd83588458d
Mihir Mehta <mihir@cs.utexas.edu>
2018-10-15 11:15:19 -0500

    Bring this proof home

--- file-system-m1.lisp
+++ file-system-m1.lisp
@@ -915,92 +915,141 @@
 (defthm character-listp-of-fat32-pathname-to-name
   (character-listp (fat32-pathname-to-name string-list)))
 
-(defun
-  m1-dir-equiv
-  (m1-file-alist1 m1-file-alist2)
+(defun m1-dir-subsetp (m1-file-alist1 m1-file-alist2)
   (declare
    (xargs
     :guard (and (m1-file-alist-p m1-file-alist1)
                 (m1-file-alist-p m1-file-alist2))
     :hints (("goal" :in-theory (enable m1-file->contents)))))
-  (b*
-      (((when (atom m1-file-alist1))
-        (atom m1-file-alist2))
+  (b* (((when (atom m1-file-alist1))
+        t)
        ((when (or (atom (car m1-file-alist1))
                   (not (stringp (car (car m1-file-alist1))))))
         (and (member-equal (car m1-file-alist1)
                            m1-file-alist2)
-             (m1-dir-equiv
+             (m1-dir-subsetp
               (cdr m1-file-alist1)
-              (remove1-equal (car m1-file-alist1) m1-file-alist2))))
+              m1-file-alist2)))
        (name (caar m1-file-alist1))
        (file1 (cdar m1-file-alist1))
        ((unless (consp (assoc-equal name m1-file-alist2)))
         nil)
        (file2 (cdr (assoc-equal name m1-file-alist2))))
-    (if
-     (not (m1-directory-file-p file1))
-     (and (not (m1-directory-file-p file2))
-          (m1-dir-equiv (cdr m1-file-alist1)
-                        (delete-assoc-equal name m1-file-alist2))
-          (equal (m1-file->contents file1)
-                 (m1-file->contents file2)))
-     (and (m1-directory-file-p file2)
-          (m1-dir-equiv (cdr m1-file-alist1)
-                        (delete-assoc-equal name m1-file-alist2))
-          (m1-dir-equiv (m1-file->contents file1)
-                        (m1-file->contents file2))))))
+    (if (not (m1-directory-file-p file1))
+        (and (not (m1-directory-file-p file2))
+             (m1-dir-subsetp (cdr m1-file-alist1) m1-file-alist2)
+             (equal (m1-file->contents file1)
+                    (m1-file->contents file2)))
+      (and (m1-directory-file-p file2)
+           (m1-dir-subsetp (cdr m1-file-alist1) m1-file-alist2)
+           (m1-dir-subsetp (m1-file->contents file1)
+                           (m1-file->contents file2))))))
 
-(defthm m1-dir-equiv-correctness-1
-  (implies (m1-dir-equiv m1-file-alist1 m1-file-alist2)
-           (equal (len m1-file-alist1)
-                  (len m1-file-alist2))))
+(defun m1-file-no-dups-p (m1-file-alist)
+  (declare
+   (xargs
+    :guard (m1-file-alist-p m1-file-alist)
+    :hints (("goal" :in-theory (enable m1-file->contents)))))
+  (cond ((atom m1-file-alist)
+         t)
+        ((not (m1-file-no-dups-p (cdr m1-file-alist)))
+         nil)
+        ((or (atom (car m1-file-alist))
+             (not (stringp (car (car m1-file-alist)))))
+         (not (member-equal (car m1-file-alist)
+                            (cdr m1-file-alist))))
+        ((assoc-equal (caar m1-file-alist) (cdr m1-file-alist))
+         nil)
+        ((m1-directory-file-p (cdar m1-file-alist))
+         (m1-file-no-dups-p (m1-file->contents (cdar m1-file-alist))))
+        (t t)))
 
-(defthm m1-dir-equiv-is-an-equivalence-lemma-1
-  (m1-dir-equiv x x))
+(defun m1-dir-equiv (m1-file-alist1 m1-file-alist2)
+  (declare (xargs :guard t))
+  (or (equal m1-file-alist1 m1-file-alist2)
+      (let ((good1 (and (m1-file-alist-p m1-file-alist1)
+                        (m1-file-no-dups-p m1-file-alist1)))
+            (good2 (and (m1-file-alist-p m1-file-alist2)
+                        (m1-file-no-dups-p m1-file-alist2))))
+        (cond ((not good1) (not good2)) ; all bad objects are equivalent
+              ((not good2) nil) ; one good, one bad; hence, not equivalent
+              (t                ; both good
+               (and (m1-dir-subsetp m1-file-alist1 m1-file-alist2)
+                    (m1-dir-subsetp m1-file-alist2 m1-file-alist1)))))))
+
+(defthm m1-dir-subsetp-preserves-assoc-equal
+  (implies (and (m1-dir-subsetp x y)
+                (stringp file)
+                (consp (assoc-equal file x)))
+           (consp (assoc-equal file y))))
 
 (defthm
-  m1-dir-equiv-is-an-equivalence-lemma-2
+  m1-dir-subsetp-transitive-lemma-1
   (implies
-   (and
-    (consp y)
-    (consp (car y))
-    (consp (assoc-equal (caar y) x))
-    (stringp (car (assoc-equal (caar y) x)))
-    (not (m1-directory-file-p (cdar y)))
-    (not (m1-directory-file-p (cdr (assoc-equal (caar y) x))))
-    (equal
-     (m1-file->contents (cdr (car y)))
-     (m1-file->contents (cdr (assoc-equal (car (car y)) x))))
-    (m1-dir-equiv (delete-assoc-equal (caar y) x)
-                  (cdr y)))
-   (m1-dir-equiv x y)))
+   (and (m1-file-alist-p y)
+        (consp (assoc-equal key y))
+        (m1-dir-subsetp y z))
+   (iff (m1-directory-file-p (cdr (assoc-equal key z)))
+        (m1-directory-file-p (cdr (assoc-equal key y)))))
+  :rule-classes
+  ((:rewrite
+    :corollary
+    (implies
+     (and (m1-file-alist-p y)
+          (consp (assoc-equal key y))
+          (m1-dir-subsetp y z)
+          (m1-directory-file-p (cdr (assoc-equal key y))))
+     (m1-directory-file-p (cdr (assoc-equal key z)))))))
 
 (defthm
-  m1-dir-equiv-is-an-equivalence-lemma-3
-  (implies (and (not (equal key nil))
-                (consp (assoc-equal key m1-file-alist1))
-                (m1-dir-equiv m1-file-alist1 m1-file-alist2))
-           (consp (assoc-equal key m1-file-alist2))))
+  m1-dir-subsetp-transitive-lemma-2
+  (implies (and (m1-file-alist-p z)
+                (m1-file-no-dups-p z)
+                (consp (assoc-equal key z))
+                (m1-directory-file-p (cdr (assoc-equal key z))))
+           (m1-file-no-dups-p
+            (m1-file->contents (cdr (assoc-equal key z))))))
 
-(thm
- (IMPLIES (AND (CONSP (ASSOC-EQUAL key y))
-               (NOT (CONSP (ASSOC-EQUAL key X)))
-               (CONSP X)
-               (M1-DIR-EQUIV (REMOVE1-EQUAL (CAR X) Y)
-                             (CDR X)))
-         (equal (ASSOC-EQUAL key y) (car x))))
+(defthm
+  m1-dir-subsetp-transitive-lemma-3
+  (implies (and (m1-file-alist-p y)
+                (consp (assoc-equal key y))
+                (m1-directory-file-p (cdr (assoc-equal key y)))
+                (m1-dir-subsetp y z))
+           (m1-dir-subsetp
+            (m1-file->contents (cdr (assoc-equal key y)))
+            (m1-file->contents (cdr (assoc-equal key z))))))
 
-(thm
- (IMPLIES (AND (CONSP X)
-               (MEMBER-EQUAL (CAR X) Y)
-               (M1-DIR-EQUIV (REMOVE1-EQUAL (CAR X) Y)
-                             (CDR X)))
-          (M1-DIR-EQUIV Y X)))
+(defthm
+  m1-dir-subsetp-transitive-lemma-4
+  (implies
+   (and (m1-file-alist-p y)
+        (consp (assoc-equal key y))
+        (not (m1-directory-file-p (cdr (assoc-equal key y))))
+        (m1-dir-subsetp y z))
+   (equal (m1-file->contents (cdr (assoc-equal key y)))
+          (m1-file->contents (cdr (assoc-equal key z))))))
 
-(thm
- (IMPLIES (M1-DIR-EQUIV X Y)
-          (M1-DIR-EQUIV Y X)))
+(defthm
+  m1-dir-subsetp-transitive
+  (implies (and (m1-file-alist-p x)
+                (m1-file-no-dups-p x)
+                (m1-file-alist-p y)
+                (m1-file-no-dups-p y)
+                (m1-file-alist-p z)
+                (m1-file-no-dups-p z)
+                (m1-dir-subsetp x y)
+                (m1-dir-subsetp y z))
+           (m1-dir-subsetp x z))
+  :hints
+  (("subgoal *1/9'''"
+    :in-theory (disable m1-dir-subsetp-transitive-lemma-1)
+    :use (:instance m1-dir-subsetp-transitive-lemma-1
+                    (key (car (car x)))))
+   ("subgoal *1/6'''"
+    :in-theory (disable m1-dir-subsetp-transitive-lemma-1)
+    :use (:instance m1-dir-subsetp-transitive-lemma-1
+                    (key (car (car x)))))))
 
 (defequiv
   m1-dir-equiv)
