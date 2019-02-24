88db2a3c58181662b9ec75b45678698164dd2d84
Mihir Mehta <mihir@cs.utexas.edu>
2019-02-24 15:55:53 +0530

    Actually change the definition of m1-dir-equiv

--- m1-dir-equiv.lisp
+++ m1-dir-equiv.lisp
@@ -78,16 +78,13 @@
 (defund m1-dir-equiv (m1-file-alist1 m1-file-alist2)
   (declare (xargs :guard (and (m1-file-alist-p m1-file-alist1)
                               (m1-file-alist-p m1-file-alist2))))
-  (or (equal m1-file-alist1 m1-file-alist2)
-      (let ((good1 (and (mbt (m1-file-alist-p m1-file-alist1))
-                        (m1-file-no-dups-p m1-file-alist1)))
-            (good2 (and (mbt (m1-file-alist-p m1-file-alist2))
-                        (m1-file-no-dups-p m1-file-alist2))))
-        (cond ((not good1) (not good2)) ; all bad objects are equivalent
-              ((not good2) nil) ; one good, one bad; hence, not equivalent
-              (t                ; both good
-               (and (m1-dir-subsetp m1-file-alist1 m1-file-alist2)
-                    (m1-dir-subsetp m1-file-alist2 m1-file-alist1)))))))
+  (b* ((good1 (and (mbt (m1-file-alist-p m1-file-alist1))
+                   (m1-file-no-dups-p m1-file-alist1)))
+       (good2 (and (mbt (m1-file-alist-p m1-file-alist2))
+                   (m1-file-no-dups-p m1-file-alist2)))
+       ((unless (and good1 good2)) (and (not good1) (not good2))))
+    (and (m1-dir-subsetp m1-file-alist1 m1-file-alist2)
+         (m1-dir-subsetp m1-file-alist2 m1-file-alist1))))
 
 (defthm m1-dir-subsetp-preserves-assoc-equal
   (implies (and (m1-dir-subsetp x y)
@@ -163,9 +160,6 @@
     :use (:instance m1-dir-subsetp-transitive-lemma-1
                     (key (car (car x)))))))
 
-(defequiv m1-dir-equiv
-  :hints (("Goal" :in-theory (enable m1-dir-equiv))))
-
 (defthm
   m1-dir-subsetp-when-atom
   (implies (atom m1-file-alist2)
@@ -237,6 +231,26 @@
   :hints (("goal" :in-theory (enable m1-directory-file-p))))
 
 (defthm
+  m1-dir-equiv-of-cons-lemma-9
+  (implies (and (m1-file-alist-p y)
+                (m1-file-no-dups-p y))
+           (m1-dir-subsetp y y))
+  :hints
+  (("goal"
+    :in-theory
+    (disable m1-dir-equiv-of-cons-lemma-4)
+    :use (:instance m1-dir-equiv-of-cons-lemma-4
+                    (x nil)))))
+
+;; A bug was here: after we changed the definition of m1-dir-equiv, we placed
+;; this defequiv form somewhat later in the file, with the result that two
+;; rules which should have rewritten in an m1-dir-equiv context instead began
+;; to rewrite in an equal context. Moving this defequiv form up here fixed the
+;; issue.
+(defequiv m1-dir-equiv
+  :hints (("Goal" :in-theory (enable m1-dir-equiv))))
+
+(defthm
   m1-dir-equiv-of-cons-lemma-6
   (implies
    (and (m1-file-alist-p fs)
@@ -295,18 +309,6 @@
                  (not (m1-file-no-dups-p (m1-file-contents-fix contents2))))
             (not (m1-dir-equiv contents1 contents2)))
    :hints (("goal" :expand (m1-dir-equiv contents1 contents2)))))
-
-(defthm
-  m1-dir-equiv-of-cons-lemma-9
-  (implies (and (m1-file-alist-p y)
-                (m1-file-no-dups-p y))
-           (m1-dir-subsetp y y))
-  :hints
-  (("goal"
-    :in-theory
-    (disable m1-dir-equiv-of-cons-lemma-4)
-    :use (:instance m1-dir-equiv-of-cons-lemma-4
-                    (x nil)))))
 
 (local
  (defthm
