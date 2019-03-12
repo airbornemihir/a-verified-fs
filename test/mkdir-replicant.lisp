1c1facce4183b95b049a21f9eb437446fbc01333
Mihir Mehta <mihir@cs.utexas.edu>
2019-03-13 05:10:45 +0530

    Refactor

--- test/mkdir-replicant.lisp
+++ test/mkdir-replicant.lisp
@@ -1,29 +1,4 @@
-(include-book "../file-system-m2")
-(include-book "../m1-syscalls")
-(include-book "centaur/getopt/top" :dir :system)
-(include-book "oslib/argv" :dir :system)
-
-(defoptions mkdir-opts
-  :parents (demo2)
-  :tag :demo2
-
-  ((parents    "no error if existing, make parent directories as needed"
-               booleanp
-               :rule-classes :type-prescription
-               :alias #\p)))
-
-(defun mkdir-list (fs name-list exit-status)
-  (b*
-      (((when (atom name-list))
-        (mv fs exit-status))
-       (fat32-pathname
-        (pathname-to-fat32-pathname (coerce (car name-list) 'list)))
-       ;; It doesn't really matter for these purposes what the errno is. We're
-       ;; not trying to match this program for its stderr output.
-       ((mv fs retval &)
-        (m1-mkdir fs fat32-pathname))
-       (exit-status (if (equal retval 0) exit-status 1)))
-    (mkdir-list fs (cdr name-list) exit-status)))
+(include-book "../test-stuff")
 
 (b*
     (((mv argv state)
