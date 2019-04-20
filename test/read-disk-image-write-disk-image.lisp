f04b45e07634213393dc8056db2dc9d20ee0f505
Mihir Mehta <mihir@cs.utexas.edu>
2019-04-20 05:12:16 -0500

    Try something

--- test/read-disk-image-write-disk-image.lisp
+++ test/read-disk-image-write-disk-image.lisp
@@ -1,14 +1,29 @@
 (include-book "../lofat")
 
+(profile 'disk-image-to-lofat)
+(profile 'lofat-to-disk-image)
+
 (b*
     (((mv & image-path state)
       (getenv$ "DISK" state))
      ((mv fat32-in-memory &)
       (time$
        (disk-image-to-lofat
-        fat32-in-memory image-path state)))
+        fat32-in-memory image-path state)
+       :msg "The execution of ~xf took ~st seconds of real ~
+                   time and ~sc seconds of run time (cpu time), and ~
+                   allocated ~sa bytes.  In an unrelated note, bar ~
+                   currently has the value ~x0.~%"
+       :args nil))
      (state
       (time$
        (lofat-to-disk-image
-        fat32-in-memory image-path state))))
+        fat32-in-memory image-path state)
+       :msg "The execution of ~xf took ~st seconds of real ~
+                   time and ~sc seconds of run time (cpu time), and ~
+                   allocated ~sa bytes.  In an unrelated note, bar ~
+                   currently has the value ~x0.~%"
+       :args nil)))
   (mv fat32-in-memory state))
+
+(memsum)
