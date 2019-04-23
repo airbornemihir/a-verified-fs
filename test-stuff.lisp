155b0ba5c7e46b538ac63b168f745da255590c8b
Mihir Mehta <mihir@cs.utexas.edu>
2019-04-23 17:18:10 +0530

    Verify a bunch of guards

--- test-stuff.lisp
+++ test-stuff.lisp
@@ -84,22 +84,65 @@
           :rule-classes :type-prescription
           :alias #\w)))
 
-(defun wc-helper (text nl nw nc beginning-of-word-p pos)
-  (declare (xargs :measure (nfix (- (length text) pos))))
-  (if
-      (zp (- (length text) pos))
+(defun
+  wc-helper
+  (text nl nw nc beginning-of-word-p pos)
+  (declare (xargs :measure (nfix (- (length text) pos))
+                  :guard (and (stringp text)
+                              (natp pos)
+                              (natp nc)
+                              (natp nw)
+                              (natp nl)
+                              (<= pos (length text)))))
+  (if (zp (- (length text) pos))
       (mv nl nw nc)
-    (b*
-        ((c (char text pos))
-         (nc (+ nc 1))
-         (nl (if (equal c #\newline) (+ nl 1) nl))
-         ((mv beginning-of-word-p nw)
-          (if (or (equal c #\space) (equal c #\newline) (equal c #\tab))
-              (mv t nw)
-            (if beginning-of-word-p
-                (mv nil (+ nw 1))
-              (mv beginning-of-word-p nw)))))
-      (wc-helper text nl nw nc beginning-of-word-p (+ pos 1)))))
+      (b* ((c (char text pos))
+           (nc (+ nc 1))
+           (nl (if (equal c #\newline) (+ nl 1) nl))
+           ((mv beginning-of-word-p nw)
+            (if (or (equal c #\space)
+                    (equal c #\newline)
+                    (equal c #\tab))
+                (mv t nw)
+                (if beginning-of-word-p (mv nil (+ nw 1))
+                    (mv beginning-of-word-p nw)))))
+        (wc-helper text nl
+                   nw nc beginning-of-word-p (+ pos 1)))))
+
+(defthm
+  wc-guard-lemma-1
+  (implies (lofat-regular-file-p file)
+           (not (lofat-directory-file-p file)))
+  :hints (("goal" :in-theory (enable lofat-directory-file-p
+                                     lofat-regular-file-p))))
+
+(defun wc-1 (fat32-in-memory pathname)
+  (declare (xargs :stobjs fat32-in-memory
+                  :guard (and (stringp pathname) (lofat-fs-p fat32-in-memory))
+                  :guard-debug t))
+  (b*
+      ((fat32-pathname (pathname-to-fat32-pathname (coerce pathname 'list)))
+       ;; It would be nice to eliminate this check by proving a theorem, but
+       ;; it's not at all simple to ensure that a string given to us is free of
+       ;; filenames indicating deleted files and such which are not allowed in
+       ;; a pathname satisfying fat32-filename-list-p.
+       ((unless (fat32-filename-list-p fat32-pathname))
+        (mv 0 0 0 1))
+       ((mv val error-code &)
+        (lofat-lstat fat32-in-memory fat32-pathname))
+       ((unless (equal error-code 0))
+        (mv 0 0 0 error-code))
+       (file-length (struct-stat->st_size val))
+       ((mv fd-table file-table fd &)
+        (lofat-open fat32-pathname fat32-in-memory nil nil))
+       ((mv file-text file-read-length &)
+        (lofat-pread
+         fd file-length 0 fat32-in-memory fd-table file-table))
+       ((unless (equal file-read-length file-length))
+        (mv 0 0 0 1))
+       ((mv nl nw nc)
+        (wc-helper file-text 0 0 0 t 0)))
+    (mv nl nw nc 0)))
 
 (defun compare-disks (image-path1 image-path2 fat32-in-memory state)
   (declare (xargs :stobjs (fat32-in-memory state)
