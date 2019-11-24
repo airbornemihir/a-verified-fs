2a0d7202e7e42156245d21865ba2cb2331567b1d
Mihir Mehta <mihir@cs.utexas.edu>
2019-11-24 19:09:18 +0530

    Improve two lemmata

--- fat32.lisp
+++ fat32.lisp
@@ -296,12 +296,20 @@
   :hints (("goal" :in-theory (enable fat32-entry-p)))
   :rule-classes :forward-chaining)
 
-;; There's an obvious generalisation of this theorem, but it requires a silly
-;; arithmetic subgoal to be resolved.
-(defthm fat32-entry-p-of-nth
-  (implies (and (fat32-entry-list-p l)
-                (< (nfix n) (len l)))
-           (fat32-entry-p (nth n l))))
+(encapsulate
+  ()
+
+  (local (include-book "arithmetic-5/top" :dir :system))
+
+  (local
+   (defthm fat32-entry-p-of-nth-lemma-1
+     (equal (< (+ -1 n) (len (cdr l)))
+            (< n (+ 1 (len (cdr l)))))))
+
+  (defthm fat32-entry-p-of-nth
+    (implies (fat32-entry-list-p l)
+             (equal (fat32-entry-p (nth n l))
+                    (< (nfix n) (len l))))))
 
 (defund
   fat32-update-lower-28
