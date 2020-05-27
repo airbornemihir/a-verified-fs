6e87c786ce99e96185222369d37c0adee9288da0
Mihir Mehta <mihir@cs.utexas.edu>
2020-05-27 16:39:43 -0500

    Remove a bunch of book inclusions

--- fat32-in-memory.lisp
+++ fat32-in-memory.lisp
@@ -8,7 +8,6 @@
 (include-book "fat32")
 (include-book "std/lists/resize-list" :dir :system)
 (include-book "std/typed-lists/unsigned-byte-listp" :dir :system)
-(local (include-book "std/lists/take" :dir :system))
 
 (make-event
  `(defstobj fat32-in-memory
