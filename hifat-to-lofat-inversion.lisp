f34d25ca86cd825bcd5f3e8373c7e8979e6d925e
Mihir Mehta <mihir@cs.utexas.edu>
2020-10-18 04:37:58 -0500

    Refactor

--- hifat-to-lofat-inversion.lisp
+++ hifat-to-lofat-inversion.lisp
@@ -7,7 +7,7 @@
 
 (include-book "generate-index-list")
 (include-book "hifat/hifat-entry-count")
-(include-book "lofat/update-data-region")
+(include-book "lofat/stobj-find-n-free-clusters")
 
 ;; These are some rules from other books which are either interacting badly
 ;; with the theory I've built up so far, or else causing a lot of unnecessary
@@ -103,66 +103,6 @@
                           (nfix (- length cluster-size)))))
     (mv (list* masked-current-cluster tail-index-list)
         tail-error)))
-
-(defund-nx
-  effective-fat (fat32-in-memory)
-  (declare
-   (xargs :stobjs fat32-in-memory
-          :guard (lofat-fs-p fat32-in-memory)
-          :guard-hints
-          (("goal" :in-theory (enable fat32-in-memoryp)))))
-  (take (+ (count-of-clusters fat32-in-memory)
-           *ms-first-data-cluster*)
-        (nth *fati* fat32-in-memory)))
-
-(defthm len-of-effective-fat
-  (equal (len (effective-fat fat32-in-memory))
-         (nfix (+ (count-of-clusters fat32-in-memory)
-                  *ms-first-data-cluster*)))
-  :hints (("goal" :in-theory (enable effective-fat))))
-
-(defthm
-  fat32-entry-list-p-of-effective-fat
-  (implies (and (fat32-in-memoryp fat32-in-memory)
-                (<= (+ (count-of-clusters fat32-in-memory)
-                       *ms-first-data-cluster*)
-                    (fat-length fat32-in-memory)))
-           (fat32-entry-list-p (effective-fat fat32-in-memory)))
-  :hints
-  (("goal" :in-theory (enable effective-fat
-                              fat-length fat32-in-memoryp)))
-  :rule-classes
-  ((:rewrite
-    :corollary
-    (implies (lofat-fs-p fat32-in-memory)
-             (fat32-entry-list-p (effective-fat fat32-in-memory))))))
-
-(defthm
-  nth-of-effective-fat
-  (equal (nth n (effective-fat fat32-in-memory))
-         (if (< (nfix n)
-                (nfix (+ (count-of-clusters fat32-in-memory)
-                         *ms-first-data-cluster*)))
-             (fati n fat32-in-memory)
-           nil))
-  :hints (("goal" :in-theory (enable fati effective-fat nth))))
-
-(defthm
-  effective-fat-of-update-data-regioni
-  (equal
-   (effective-fat (update-data-regioni i v fat32-in-memory))
-   (effective-fat fat32-in-memory))
-  :hints (("goal" :in-theory (enable effective-fat))))
-
-(defthm
-  effective-fat-of-update-fati
-  (equal (effective-fat (update-fati i v fat32-in-memory))
-         (if (< (nfix i)
-                (+ (count-of-clusters fat32-in-memory)
-                   *ms-first-data-cluster*))
-             (update-nth i v (effective-fat fat32-in-memory))
-           (effective-fat fat32-in-memory)))
-  :hints (("goal" :in-theory (enable effective-fat update-fati))))
 
 (defthm
   get-clusterchain-alt
@@ -1836,98 +1776,6 @@
                            (mv-nth 0 (root-dir-ent-list fat32-in-memory))
                            (max-entry-count fat32-in-memory))))))
     :hints (("Goal" :in-theory (enable lofat-to-hifat)) ))))
-
-(defund
-  stobj-find-n-free-clusters-helper
-  (fat32-in-memory n start)
-  (declare
-   (xargs
-    :guard (and (lofat-fs-p fat32-in-memory)
-                (natp n)
-                (natp start))
-    :stobjs fat32-in-memory
-    :measure (nfix (- (+ (count-of-clusters fat32-in-memory)
-                         *ms-first-data-cluster*)
-                      start))))
-  (if
-      (or (zp n)
-          (mbe :logic (zp (- (+ (count-of-clusters fat32-in-memory)
-                                *ms-first-data-cluster*)
-                             start))
-               :exec (>= start
-                         (+ (count-of-clusters fat32-in-memory)
-                            *ms-first-data-cluster*))))
-      nil
-    (if
-        (not (equal (fat32-entry-mask (fati start fat32-in-memory))
-                    0))
-        (stobj-find-n-free-clusters-helper
-         fat32-in-memory n (+ start 1))
-      (cons
-       (mbe :exec start :logic (nfix start))
-       (stobj-find-n-free-clusters-helper fat32-in-memory (- n 1)
-                                          (+ start 1))))))
-
-(defthm
-  nat-listp-of-stobj-find-n-free-clusters-helper
-  (nat-listp
-   (stobj-find-n-free-clusters-helper fat32-in-memory n start))
-  :hints
-  (("goal"
-    :in-theory (enable stobj-find-n-free-clusters-helper)))
-  :rule-classes
-  (:rewrite
-   (:rewrite
-    :corollary (integer-listp (stobj-find-n-free-clusters-helper
-                               fat32-in-memory n start)))))
-
-(defthm
-  stobj-find-n-free-clusters-helper-correctness-1
-  (implies
-   (and (natp start)
-        (lofat-fs-p fat32-in-memory))
-   (equal
-    (stobj-find-n-free-clusters-helper fat32-in-memory n start)
-    (find-n-free-clusters-helper
-     (nthcdr start (effective-fat fat32-in-memory))
-     n start)))
-  :hints
-  (("goal" :in-theory (enable stobj-find-n-free-clusters-helper
-                              find-n-free-clusters-helper)
-    :induct (stobj-find-n-free-clusters-helper
-             fat32-in-memory n start))))
-
-(defund
-  stobj-find-n-free-clusters
-  (fat32-in-memory n)
-  (declare
-   (xargs :guard (and (lofat-fs-p fat32-in-memory)
-                      (natp n))
-          :stobjs fat32-in-memory))
-  (stobj-find-n-free-clusters-helper
-   fat32-in-memory n *ms-first-data-cluster*))
-
-(defthm
-  nat-listp-of-stobj-find-n-free-clusters
-  (nat-listp (stobj-find-n-free-clusters fat32-in-memory n))
-  :hints
-  (("goal" :in-theory (enable stobj-find-n-free-clusters)))
-  :rule-classes
-  (:rewrite
-   (:rewrite
-    :corollary (integer-listp (stobj-find-n-free-clusters-helper
-                               fat32-in-memory n start)))))
-
-(defthm
-  stobj-find-n-free-clusters-correctness-1
-  (implies
-   (lofat-fs-p fat32-in-memory)
-   (equal (stobj-find-n-free-clusters fat32-in-memory n)
-          (find-n-free-clusters (effective-fat fat32-in-memory)
-                                n)))
-  :hints (("goal" :in-theory (enable stobj-find-n-free-clusters
-                                     find-n-free-clusters)))
-  :rule-classes :definition)
 
 (defthm
   stobj-set-indices-in-fa-table-guard-lemma-1
