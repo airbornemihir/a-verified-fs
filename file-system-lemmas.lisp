1596321b6bd1dd60f9b1c2dfc7aca4b9acdc24ea
Mihir Mehta <mihir@cs.utexas.edu>
2021-03-29 12:58:35 -0500

    Accept updates from upstream

--- file-system-lemmas.lisp
+++ file-system-lemmas.lisp
@@ -1700,7 +1700,8 @@
          (if (zp n)
              nil
              (append (take (- n 1) l)
-                     (list (nth (- n 1) l))))))
+                     (list (nth (- n 1) l)))))
+  :rule-classes ((:definition :install-body nil)))
 
 (theory-invariant (incompatible (:rewrite take-as-append-and-nth)
                                 (:rewrite append-of-take-and-cons)))
